6646e70ab2ffd247e2b43c06e7ce6796167f19fd
highchen147 <steverogersavengers@gmail.com>
2023-04-16 17:47:54 -0600

    ya se aplica condicion inicial

--- burgers2DVF/burgers2DVF.cpp
+++ burgers2DVF/burgers2DVF.cpp
@@ -17,7 +17,8 @@ double gauss(double x, double y);
 double step_neg(double x, double y);
 double step_pos(double x, double y);
 double gauss_impar(double x, double y);
-void salida(ofstream &of, double *u, double *x, double t, int N);
+double funcion_cond_inicial(const string &nombre_funcion, double x, double y);
+void salida(ostream &of, double **u, double *x, double *y, double t, int Nx, int Ny);
 double FlujoBurgers(double u);                        
 double Flujo(double u, double v, const string &Marco, double dx, double dt);
 double uPrime(double u, double v);
@@ -38,8 +39,8 @@ int main()
     double tiempo = 0.0; // Variable de tiempo en la simulación
 
     // Parámetros espaciales
-    int Nx = 500; // Número de puntos en el eje x
-    int Ny = 500; // Número de puntos en el eje y
+    int Nx = 200; // Número de puntos en el eje x
+    int Ny = 200; // Número de puntos en el eje y
     double Lx = 100.0; // Largo del dominio x en metros
     double Ly = 100.0; // Largo del dominio y en metros
     double dx = Lx/(Nx-1); // Tamaño de paso en el eje x
@@ -52,22 +53,22 @@ int main()
     // step_pos: Función Heaviside valuada en L/2+x
     // gauss: Función gaussiana centrada en L/2 con amplitud A
     // gauss_impar: Función gaussiana multiplicada por (x-L/2) con amplitud A
-    string funcion_inicial;
+    string nombre_func_inicial;
     cout << "Escriba la condición inicial a utilizar, puede ser:"<< endl; 
     cout << "step_neg \nstep_pos \ngauss \ngauss_impar \ngauss_neg" << endl;
     cout << ">> ";
-    cin  >> funcion_inicial;
+    cin  >> nombre_func_inicial;
     cout << endl;
-    while (funcion_inicial != "step_neg" && 
-           funcion_inicial != "step_pos" && 
-           funcion_inicial != "gauss" && 
-           funcion_inicial != "gauss_impar" &&
-           funcion_inicial != "gauss_neg") 
+    while (nombre_func_inicial != "step_neg" && 
+           nombre_func_inicial != "step_pos" && 
+           nombre_func_inicial != "gauss" && 
+           nombre_func_inicial != "gauss_impar" &&
+           nombre_func_inicial != "gauss_neg") 
     {
         cout << "Función inválida. Intente de nuevo con:" << endl;
         cout << "step_neg \nstep_pos \ngauss \ngauss_impar" << endl;
         cout << ">> ";
-        cin >> funcion_inicial;
+        cin >> nombre_func_inicial;
         cout << "\n" << endl;
     }
 
@@ -91,7 +92,7 @@ int main()
 
     // Nombre del archivo con los datos
     string nombreDatos;
-    nombreDatos = funcion_inicial + "-" + tipo_frontera + ".dat";
+    nombreDatos = nombre_func_inicial + "-" + tipo_frontera + ".dat";
     // Se abre el archivo donde van los datos
     ofstream outfile;
     outfile.open(nombreDatos, ios::out);
@@ -112,7 +113,7 @@ int main()
         // Función u
         u[i] = new double[Ny];
         u_nueva[i] = new double[Ny];
-        // Arreglos dimensionales
+        // Arreglos de dominio x
         x[i] = i*dx;
     }
     // Arreglo de dominio y
@@ -120,15 +121,23 @@ int main()
     {
         y[j] = j*dx;
     }
-    
-
+    // Aplicar condición inicial a u
+    for (int i = 0; i < Nx; i++)
+    {
+        for (int j = 0; j < Ny; j++)
+        {
+            u[i][j] = funcion_cond_inicial(nombre_func_inicial, x[i], y[j]);
+        }
+        
+    }
+    salida(outfile, u, x, y, 0, Nx, Ny);
     
 }
 
 void salida(ostream &of, double **u, double *x, double *y, double t, int Nx, int Ny )
 {
-  for( int i=0; i<Nx+1; i++ ){
-    for( int j=0; j<Ny+1; j++ ){
+  for( int i=0; i < Nx; i++ ){
+    for( int j=0; j < Ny; j++ ){
         of << t << "\t" << x[i] << "\t" << y[j] << "\t" << u[i][j] << endl;
     }
     of << endl;
@@ -143,6 +152,13 @@ double gauss(double x, double y)
     double y0 = 50; 
     double A = 3.5;
     return A*exp(-b*(pow(x - x0,2) + pow(y-y0,2)));
+}
+
+double gauss_impar(double x, double y)
+{
+    double a = 0.4;
+    double L = 100;
+    return a*(x-L/2)*gauss(x, y);
 }
 
 double step_neg(double x, double y)
@@ -171,6 +187,28 @@ double step_pos(double x, double y)
     {
         return 0.0;
     }
+}
+
+double funcion_cond_inicial(const string &nombre_funcion, double x, double y)
+{
+    if (nombre_funcion == "gauss")
+    {
+        return gauss(x, y);
+    }
+    else if (nombre_funcion == "step_neg")
+    {
+        return step_neg(x, y);
+    }
+    else if (nombre_funcion == "step_pos")
+    {
+        return step_pos(x, y);
+    }
+    else if (nombre_funcion == "gauss_impar")
+    {
+        return gauss_impar(x, y);
+    }
+    else 
+        return gauss(x, y);
 }
 
 void imprimir(ofstream &file, double xcor, double ycor, double f)
